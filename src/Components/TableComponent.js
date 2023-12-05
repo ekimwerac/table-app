bf65f7545869fc0c30219293611fed859002abaa
Mike Carew <mike@carew.uk>
2023-12-05 12:15:46 +0000

    EditRowImplicitSave

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import TableRow from './TableRow';
-import { fetchSellers, updateSeller } from '../../services/api';
+import { fetchSellers, updateSeller } from '../Services/Api';
 
 const TableComponent = () => {
   const [sellers, setSellers] = useState([]);
